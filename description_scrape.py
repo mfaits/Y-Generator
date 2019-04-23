180f99d799682fb606777fb5098551a839b0b841
Michelle Faits <michelle.faits@graybar.com>
2019-04-23 17:54:10 -0500

    successfully scrapes YC company descriptions and saves CSV in Data folder

--- description_scrape.py
+++ description_scrape.py
@@ -1,11 +1,32 @@
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
+import pandas as pd
 
 # specify the url
 company_page = 'https://www.ycombinator.com/companies/'
 
-# query the website and return the html to the variable ‘page’
-page = requests.get(company_page)
+# YC page loads table with a javascript call so access it using selenium
+# browser instead of loading it straight into soup
+browser = webdriver.Chrome()
+browser.get(company_page)
 
-# parse the html using beautiful soup and store in variable `soup`
-soup = BeautifulSoup(page.text, 'html.parser')
+# returns the inner HTML as a string and gives it to soup
+innerHTML = browser.execute_script("return document.body.innerHTML") 
+soup = BeautifulSoup(innerHTML)
+
+table = soup.table
+table_rows = table.find_all('tr')
+
+descriptions = []
+
+for tr in table_rows:
+    td = tr.find_all('td')
+    row = [i.text for i in td]
+    descriptions.append(row[2])
+
+df_descriptions = pd.DataFrame(descriptions)
+
+df_descriptions.to_csv('Data\\company_descriptions.csv',
+                        header=False,
+                        index=False)
